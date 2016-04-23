f314c69d2e8173f6866e12da0cb166609ad5cc82
ajaini <anki.jni@gmail.com>
2016-04-23 15:13:05 +0530

    changes to script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('So you want to talk to Ankit? Just say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                    return bot.say(`Trying to be adventurous eh? I'm a mere bot. Stick to the script! What's your mood like?`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
